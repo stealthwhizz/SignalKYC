f4cdc7a3264c195cd7176241fb3df2eb6142dec7
JEAN REGIS <240509606@firat.edu.tr>
2026-05-18 18:00:08 +0300

    Update investigationStore.ts

--- store/investigationStore.ts
+++ store/investigationStore.ts
@@ -1,1 +1,21 @@
+// store/investigationStore.ts
+import { create } from 'zustand'
+import { Investigation, AgentStep } from '@/types'
 
+interface InvestigationState {
+  currentInvestigation: Investigation | null
+  activeStepIndex: number
+  isLoading: boolean
+  setCurrentInvestigation: (inv: Investigation) => void
+  setActiveStepIndex: (idx: number) => void
+  setIsLoading: (loading: boolean) => void
+}
+
+export const useInvestigationStore = create<InvestigationState>((set) => ({
+  currentInvestigation: null,
+  activeStepIndex: 0,
+  isLoading: false,
+  setCurrentInvestigation: (inv) => set({ currentInvestigation: inv, activeStepIndex: 0 }),
+  setActiveStepIndex: (idx) => set({ activeStepIndex: idx }),
+  setIsLoading: (loading) => set({ isLoading: loading }),
+}))
